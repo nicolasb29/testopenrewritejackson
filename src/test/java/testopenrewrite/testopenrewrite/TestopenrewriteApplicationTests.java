666a69e68fc065052f3f30e01837e9464be227bb
nicolasb29 <nicolas.bonamy2@gmail.com>
2024-11-28 19:34:11 +0100

    first commit

---
+++ src/test/java/testopenrewrite/testopenrewrite/TestopenrewriteApplicationTests.java
@@ -0,0 +1,13 @@
+package testopenrewrite.testopenrewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestopenrewriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
